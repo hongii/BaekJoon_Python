40efad8c303cedcd5d0b8b01312583120a3e3f55
khe <khj778878@naver.com>
2023-09-08 18:00:08 +0900

    [약점 체크]: 부분합(1806번)_2회차

--- 약점 체크/부분합(1806번).py
+++ 약점 체크/부분합(1806번).py
@@ -1,27 +1,29 @@
-# 투 포인터 문제
-import sys
-filePath = "C:\\Users\\cywoo\\OneDrive\\바탕 화면\\Python\\백준\\BaekJoon\\약점 체크\\input.txt"
-sys.stdin = open(filePath, "rt")
-n, s = map(int, input().split())
-numList = list(map(int, input().split()))
+### 투 포인터 문제 ###
 
-lp, rp = 0, 0
-tmpSum, res = 0, 100001 # tmpSum은 nunmList[lp:rp]까지의 합(numList[rp]의 값은 포함하지 않음!)
-while True:
-    if tmpSum >= s:
-        res = min(res, rp-lp)
-        tmpSum -= numList[lp]
-        lp += 1
-    elif rp == n:
-        break
-    else:
-        tmpSum += numList[rp]
-        rp += 1
+# 1회차 코드
+# import sys
+# filePath = "C:\\Users\\cywoo\\OneDrive\\바탕 화면\\Python\\백준\\BaekJoon\\약점 체크\\input.txt"
+# sys.stdin = open(filePath, "rt")
+# n, s = map(int, input().split())
+# numList = list(map(int, input().split()))
 
-if res == 100001:
-    print(0)
-else:
-    print(res)
+# lp, rp = 0, 0
+# tmpSum, res = 0, 100001 # tmpSum은 nunmList[lp:rp]까지의 합(numList[rp]의 값은 포함하지 않음!)
+# while True:
+#     if tmpSum >= s:
+#         res = min(res, rp-lp)
+#         tmpSum -= numList[lp]
+#         lp += 1
+#     elif rp == n:
+#         break
+#     else:
+#         tmpSum += numList[rp]
+#         rp += 1
+
+# if res == 100001:
+#     print(0)
+# else:
+#     print(res)
 
 '''
 PS. (아....문제 제대로 좀 읽자................)
@@ -32,3 +34,27 @@ PS. (아....문제 제대로 좀 읽자................)
 그리고 부분합 초기값 설정에 따라 tmpSum을 비교하는 순서가 중요!
 해당 문제 다시 풀어보기
 '''
+
+####################################################################################################################
+# 2회차 코드
+import sys
+filePath = "C:\\Users\\cywoo\\OneDrive\\바탕 화면\\Python\\백준\\BaekJoon\\약점 체크\\input.txt"
+sys.stdin = open(filePath, "rt")
+input = sys.stdin.readline
+n, s = map(int, input().split())
+numList = list(map(int, input().split()))
+
+lp, rp = 0, 0
+tmpSum, res = 0, n
+
+for lp in range(n):
+    while tmpSum < s and rp < n:
+        tmpSum += numList[rp]
+        rp += 1
+
+    if tmpSum >= s and res > rp - lp:
+        res = rp - lp
+
+    tmpSum -= numList[lp]
+
+print(res)
